309abe776ccbf358bd123ab244cc2c240c9ee6cf
OmniFab <omnifabrakotondrajaona@gmail.com>
2026-07-11 19:58:16 +0300

    New modification

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -6,7 +6,9 @@ export default function PriceComponent(props){
     //render
     return (<>
         <div className={fruitsOptions.priceContent}>
-            <p className={props.isTrue ? styles.label : styles.labelFalse}>{props.labelPrice}</p>
+            <p className={props.isTrue ? styles.label : styles.labelFalse}>
+                {props.labelPrice}
+            </p>
             <span 
                 style={{
                     color: props.color,
